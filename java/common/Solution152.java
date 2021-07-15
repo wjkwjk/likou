149a6c6e146f9c418a885ed7ff84cc66bfc11799
wjkwjk <1838165617@qq.com>
2021-07-15 17:03:09 +0800

    7.15，增加152

---
+++ java/common/Solution152.java
@@ -0,0 +1,59 @@
+package common;
+
+import java.util.ArrayList;
+
+/**
+ * 152. 乘积最大子数组
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ */
+public class Solution152 {
+    public int maxProduct(int[] nums) {
+        int maxn = Integer.MIN_VALUE;
+        ArrayList<Integer> list = new ArrayList<>();
+        int lastp = 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]<0)  list.add(i);
+            if (nums[i]==0){
+                maxn = Math.max(0, maxn);
+                if (list.size()%2==0){
+                    int t = 1;
+                    for (int j = lastp;j<i;j++){
+                        t*=nums[j];
+                    }
+                    maxn = Math.max(maxn, t);
+                }else{
+                    int t = 1;
+                    for (int j = list.get(0)+1;j<list.get(list.size()-1);j++)   t*=nums[j];
+                    int temp = t;
+                    for (int j=lastp;j<=list.get(0);j++)    temp*=nums[j];
+                    maxn = Math.max(maxn, temp);
+                    temp = t;
+                    for (int j = list.get(list.size()-1); j<i;j++)  temp*=nums[j];
+                    maxn = Math.max(maxn, temp);
+                }
+                lastp = i+1;
+                list.clear();
+            }
+        }
+
+        if (list.size()%2==0){
+            int t = 1;
+            for (int j = lastp;j<nums.length;j++){
+                t*=nums[j];
+            }
+            maxn = Math.max(maxn, t);
+        }else{
+            int t = 1;
+            for (int j = list.get(0)+1;j<list.get(list.size()-1);j++)   t*=nums[j];
+            int temp = t;
+            for (int j=lastp;j<=list.get(0);j++)    temp*=nums[j];
+            maxn = Math.max(maxn, temp);
+            temp = t;
+            for (int j = list.get(list.size()-1); j<nums.length;j++)  temp*=nums[j];
+            maxn = Math.max(maxn, temp);
+        }
+        return maxn;
+    }
+
+
+}
